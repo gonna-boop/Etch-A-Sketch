6e87accc2e2119129959942a558927f67db05ea1
Chris F <93630113+gonna-boop@users.noreply.github.com>
2022-11-11 21:58:51 -0700

    change div size, edit for loop

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
-for (let i=0; i<16; i++) {
-    const container = document.querySelector('#gameBoard');
+const container = document.querySelector('#gameBoard');
+
+let rowTot = 16;
+let cellTot = rowTot * rowTot;
+
+for (let i=1; i<257; i++) {
     let div = document.createElement('div');
-        div.style.width = "100px";
-        div.style.height = "100px";
+        div.style.width = "25px";
+        div.style.height = "25px";
         div.style.color = "white";
     
     div.addEventListener('mouseover', e => e.target.classList.add(
