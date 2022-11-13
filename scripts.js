78f0a0346d8b69bcc5031d1d26ea435537551ab3
Chris F <93630113+gonna-boop@users.noreply.github.com>
2022-11-13 15:10:49 -0700

    change loop to not function until a user prompt is entered

--- scripts.js
+++ scripts.js
@@ -1,16 +1,30 @@
-const container = document.querySelector('#gameBoard');
+button.addEventListener("click", promptUser);
 
-let rowTot = 16;
-let cellTot = rowTot * rowTot;
-
-for (let i=1; i<257; i++) {
-    let div = document.createElement('div');
-        div.style.width = "25px";
-        div.style.height = "25px";
-        div.style.color = "white";
-    
-    div.addEventListener('mouseover', e => e.target.classList.add(
-        'first-color-class'));
-    
-    document.getElementById("gameBoard").appendChild(div);  
+function promptUser() {
+    let rowTot = prompt("Enter the grid size you would like (2-64)");
+        const container = document.querySelector('#gameBoard');
+        for (let i=1; i<(rowTot * rowTot); i++) {
+        let div = document.createElement('div');
+            div.style.width = "25px";
+            div.style.height = "25px";
+            div.style.color = "white";
+        
+        div.addEventListener('mouseover', e => e.target.classList.add(
+            'first-color-class'));
+        
+        document.getElementById("gameBoard").appendChild(div);  
 }
+}
+
+// const container = document.querySelector('#gameBoard');
+// for (let i=1; i<rowTot; i++) {
+//     let div = document.createElement('div');
+//         div.style.width = "25px";
+//         div.style.height = "25px";
+//         div.style.color = "white";
+    
+//     div.addEventListener('mouseover', e => e.target.classList.add(
+//         'first-color-class'));
+    
+//     document.getElementById("gameBoard").appendChild(div);  
+// }
