7031bedff248909f9494ad230c8dc4654adaa6f8
Chris F <93630113+gonna-boop@users.noreply.github.com>
2022-11-15 10:23:17 -0700

    create grid on page load, create prompt user function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 button.addEventListener("click", promptUser);
 
+createGrid();
+function createGrid(){
+    const container = document.querySelector('#gameBoard');
+    for (let i=1; i<257; i++) {
+        let div = document.createElement('div');
+            div.style.width = "25px";
+            div.style.height = "25px";
+            div.style.color = "white";
+        
+        div.addEventListener('mouseover', e => e.target.classList.add(
+            'first-color-class'));
+        
+        document.getElementById("gameBoard").appendChild(div);  
+    }
+}
+
 function promptUser() {
     let rowTot = prompt("Enter the grid size you would like (2-64)");
         const container = document.querySelector('#gameBoard');
@@ -15,16 +31,3 @@ function promptUser() {
         document.getElementById("gameBoard").appendChild(div);  
 }
 }
-
-// const container = document.querySelector('#gameBoard');
-// for (let i=1; i<rowTot; i++) {
-//     let div = document.createElement('div');
-//         div.style.width = "25px";
-//         div.style.height = "25px";
-//         div.style.color = "white";
-    
-//     div.addEventListener('mouseover', e => e.target.classList.add(
-//         'first-color-class'));
-    
-//     document.getElementById("gameBoard").appendChild(div);  
-// }
