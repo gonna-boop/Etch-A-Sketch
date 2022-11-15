e7d23161c6542d342b41bf89c0843617fd60e463
Chris F <93630113+gonna-boop@users.noreply.github.com>
2022-11-15 13:08:38 -0700

    finish working promptUser() function
    GOT IT!!!!!

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-button.addEventListener("click", promptUser);
+// //creates the initial grid for the page, a 16x16 grid of divs that change from white 
+// //to black when scrolling over them with a mouse
+
 
 createGrid();
 function createGrid(){
@@ -16,13 +18,18 @@ function createGrid(){
     }
 }
 
+button.addEventListener("click", promptUser);
+
 function promptUser() {
     let rowTot = prompt("Enter the grid size you would like (2-64)");
         const container = document.querySelector('#gameBoard');
-        for (let i=1; i<(rowTot * rowTot); i++) {
+        while(gameBoard.lastChild){
+            gameBoard.removeChild(gameBoard.lastChild);
+        }
+        for (let i=1; i<(rowTot * rowTot)+1; i++) {
         let div = document.createElement('div');
-            div.style.width = "25px";
-            div.style.height = "25px";
+            div.style.width = `${400/rowTot}px`;
+            div.style.height = `${400/rowTot}px`;
             div.style.color = "white";
         
         div.addEventListener('mouseover', e => e.target.classList.add(
